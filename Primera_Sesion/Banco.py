0bc6e5717dc4076a9164f67056d5cf6a41fd31bc
Daniel G. Torres <danielguevara1500@gmail.com>
2024-12-03 09:42:05 -0500

    termiertrene de hacer el ejericio de la ultima clase

--- Primera_Sesion/Banco.py
+++ Primera_Sesion/Banco.py
@@ -1,30 +1,39 @@
 #Ejercicio para practicar 
 
 class Banco:
+    TASA_INTERES = 0.3  #Tasa de interes inicial
     
-    TASA_INTERES = 0.3
-    
-    def __init__(self,nombre):
+    def __init__(self, nombre):
         self.nombre = nombre
         
     @classmethod
-    def cambiarTasa(cls, nuevaTasa):
+    def cambiarTasa(cls,nuevaTasa):
         cls.TASA_INTERES = nuevaTasa
     
+    @classmethod
+    def nuevaTasaDescripcion(cls, banco):
+        return f"La nueva tasa es {cls.TASA_INTERES} con el banco {banco.nombre}"
+    
     @staticmethod
-    def conversionDolaresEuros(dolares): 
+    def conversionDolaresEuros(dolares):
         return dolares * 0.85
+    
+# Crear un obejeto de la clase Banco
 
-banco1 = Banco("Davivienda")
-print("-----------")
-print(Banco.conversionDolaresEuros)
-print("-----------")
+banco1 = Banco("Davivivenda")
+
+# Cambiar la tasa de interes 
+Banco.cambiarTasa(0.35)
+
+# Imprimir la conversion de dolares a euros
+
+print("---------------")
+print(Banco.conversionDolaresEuros(100)) #ejemplo con 100 dolares
+
+# Imprimir el nombre del banco
+print("---------------")
 print(banco1.nombre)
-print("-----------")
-print(Banco.nuevaTasaE())
 
-
-
-''' @classmethod
-    def nuevaTasaE(self):
-        return f"La nueva tasa es {self.TASA_INTERES} con el banco {banco1.nombre}"'''
+# Imprimir la nueva tasa de interes 
+print("---------------")
+print(Banco.nuevaTasaDescripcion(banco1))
